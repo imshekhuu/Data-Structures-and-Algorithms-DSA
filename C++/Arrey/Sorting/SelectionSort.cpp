19bfa89fee32e3edcf2ea4f8f88e24444f0da045
imshekhuu <divasinghwork16@gmail.com>
2026-02-08 08:16:23 +0530

    selectionsort added

---
+++ C++/Arrey/Sorting/SelectionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int arr[], int size);
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = 5;
+
+    SelectionSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+void SelectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[minIndex], arr[i]);
+    }
+}
