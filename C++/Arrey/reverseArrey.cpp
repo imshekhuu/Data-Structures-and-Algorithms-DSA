46b0419e6bc0fa4e55a2f5611d0883fae0868cab
imshekhuu <divasinghwork16@gmail.com>
2026-03-24 19:36:21 +0530

    added

---
+++ C++/Arrey/reverseArrey.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> reverse(vector<int> arr){
+    int s = 0;
+    int e = arr.size() - 1;
+
+    while(s<e){
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+
+    return arr;
+}
+
+void print(vector<int> arr){
+
+    for(int i = 0; i<arr.size(); i++){
+        cout << arr[i];
+    }
+
+    cout << " " << endl;
+}
+
+int main(){
+    vector<int> arr;
+
+    arr.push_back(3);
+    arr.push_back(7);
+    arr.push_back(8);
+    arr.push_back(5);
+    arr.push_back(4);
+
+    vector<int> and = reverse(arr);
+
+    cout << "Reversed arrey";
+    print(ans);
+}
